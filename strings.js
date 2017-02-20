4e53282756af4544b82b2d6ed85152375167a7aa
sharon smith <sharon.e.smith2@gmail.com>
2017-02-19 21:44:43 -0600

    String Manipulations / DONE

--- strings.js
+++ strings.js
@@ -1,8 +1,4 @@
-// 4. Implement the logic in the reversal function that reverses the order 
-// of the characters in the string, 
-// ... and outputs the result in the DOM, 
-// below the text input.
-//
+
 // FUNCTION RETURNS STRING IN REVERSE ORDER
 function reversal(inputStr) {
 
@@ -14,57 +10,36 @@ function reversal(inputStr) {
 };
 
 
-
-// 5. Implement the logic in the alphabits function that return the characters 
-// in alphabetical order, 
-// ... and outputs the result in the DOM, below the text input.
-//
 // FUNCTION RETURNS STRING ALPHABETIZED STRING
 function alphabits(inputStr) {
 
 	return inputStr.toLowerCase().split('').sort().join('');
-	
 };
 
 
-
-// 6. Implement the logic in the palindrome function that determine whether 
-// the string is a palindrome. If it is, 
-// ... display the text "Your string is a palidrome" 
-// in the DOM, below the text input.
-//
 // FUNCTION RETURNS <true> IF PALINDROME; <false> IF NOT
 function palindrome(inputStr) {
 
 	var inputStr = inputStr.toLowerCase();
 
 	var loopLength;
-	if (inputStr.length%2 === 0) {
-		loopLength = inputStr.length%2; 	// string has even number of elements
-	} else {
-		loopLength = inputStr.length%2 + 1; // string has odd number of elements
+	if (inputStr.length%2 === 0) {			  // string has even number of elements
+		loopLength = inputStr.length/2; 	
+	} else {								  // string has odd number of elements
+		loopLength = (inputStr.length+1) / 2; 
 	}
 
 	for (var i=0; i<loopLength; i++) {
-console.log("in palindrome for loop");
-console.log("inputStr[i] :: ", inputStr[i]);
-console.log("inputStr[inputStr.length - (i+1)]");
-		if (inputStr[i] !== inputStr[inputStr.length - (i+1)]) { // characters do not match
-console.log("inputStr[i], for i = ", i, "; ", inputStr[i]);
-console.log("inputStr[length - (i+1)] :: ", inputStr[ilength - (i+1)]);
-			return false;										 // not a palindrome
+
+		if (inputStr[i] !== inputStr[inputStr.length - (i+1)]) { 
+
+			return false; // characters do not match ; not a palindrome										 // not a palindrome
 		}
 	}
 	return true; // string is a palindrome
 };
 
 
-
-
-// 7. When the user presses the enter key in the text input, or clicks the button, 
-// set the value of the testString variable (see below) to the value of the input.
-
-// 8. The output of each of the functions should immediately appear as well.
 
 //*************************************************
 // EVENT HANDLERS
@@ -107,7 +82,7 @@ function onlyAlpha () {
 	for (var i=0; i< inputStr.length; i++) {
 		if (!alphaStr.includes(inputStr[i])) { // not alpha char
 			alert("You must enter alpha characters only.")
-			document.getElementById("inputStr").value = "";
+			document.getElementById("inputStr").value = ""; // reset user input field
 			return false;
 		}
 	}
@@ -117,10 +92,10 @@ function onlyAlpha () {
 
 function runMain() {
 	var testString = document.getElementById("inputStr").value;
-	var reversed; 
-	var alphabetized;
-	var isPalindrome;
-	var palindromeStr;
+	var reversed; 		// string, returned from function
+	var alphabetized;	// string, returned from function
+	var isPalindrome;	// <true/false>, returned from function
+	var palindromeStr;	// output string
 	var outputText = document.getElementById("output");
 
 	reversed = reversal(testString);
